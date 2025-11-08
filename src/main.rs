993f8f7a42c983e6301fe5b1c6ff4e13e82e6551
Samuele Lombardi <59124097+Dieal@users.noreply.github.com>
2025-11-08 18:38:33 +0100

    feat: open buffer from reading path from CLI

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
-use std::{io, thread::sleep, time::Duration};
+use std::{env, fs, io, thread::sleep, time::Duration};
 
 use crossterm::terminal::enable_raw_mode;
-use text_editor::{editor::Editor, screen::Screen, Buffer, Cell};
+use text_editor::{editor::Editor, screen::Screen};
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
 
-    let text = String::from("Prova 12345");
     let mut editor = Editor::new()?;
-    editor.add_buffer_from_text(text);
-    editor.set_screen_buffer_from_index(0);
-    editor.draw_current_buffer();
-    Screen::flush();
+    let mut args = env::args();
+    args.next(); // Skips binary name
+    let args: Vec<String> = args.collect();
+
+    if !args.is_empty() {
+        let path = args.first().expect("Should have first argument");
+        if let Ok(text) = fs::read_to_string(path) {
+            editor.add_buffer_from_text(text);
+            editor.set_screen_buffer_from_index(0);
+            editor.draw_current_buffer();
+        } else {
+            print!("File {path} not found");
+        }
+        Screen::flush();
+    }
     sleep(Duration::from_secs(3));
 
     Ok(())
